35a57b3fd61537e5fdb85abd0b0d4af5ed7f28c6
wangui.njoka <wangui.njoka@gmail.com>
2018-08-23 16:55:27 +0300

    created class user and initiazed it

---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+    '''
+    class that generates new instances of users
+
+    '''
+    def __init__(self, username,password):
+
+        self.username = username
+        self.password = password
