e11e55e1b1b826f2124c5261ef85bd74f2c6301e
wangui.njoka <wangui.njoka@gmail.com>
2018-08-24 08:49:33 +0300

    added class credentials and initialized it and created test file

--- user.py
+++ user.py
@@ -7,3 +7,15 @@ class User:
 
         self.username = username
         self.password = password
+
+class Credentials(User):
+
+    '''
+    class that generates new instances of Credentials
+
+    '''
+    def __init__(self, appName, loginname, passwordUsed)
+
+        self.appName = appName
+        self.loginname = loginname
+        self.passwordUsed = passwordUsed
