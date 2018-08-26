25b21aeef5a2973d956edc4a4b6b5a91cc305704
wangui.njoka <wangui.njoka@gmail.com>
2018-08-26 23:53:56 +0300

    created a function to create new user

--- password_locker.py
+++ password_locker.py
@@ -1,3 +1,10 @@
 import pyperclip
 from user import User
 from user import Credentials
+
+def create_user(username,password):
+    '''
+    function that creates a new user to the app
+    '''
+    new_user = User(username,password)
+    return new_user
